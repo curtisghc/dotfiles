5f3ebd2df7ac326b73f34454c111fdeb458c8085
Curtis Haig Gallagher <curtisghc@gmail.com>
2018-02-17 21:41:40 -0500

    last commit

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,15 +1,34 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
 
+;;packages listed
+(setq package-list '(evil flycheck yasnippet auto-complete
+						  auto-complete-clang-async evil-org haskell-mode
+						  haskell-snippets python-environment magit
+						  gnu-apl-mode expand-region gruvbox-theme))
+;;auto install packages
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (unless (package-installed-p package)
+	(package-install package)))
+
+;;theme
+(load-theme 'gruvbox-light-soft t)
+;;(tool-bar-mode nil)
+
+;;qol updates
 (fset 'yes-or-no-p 'y-or-n-p)
+
 (show-paren-mode t)
 (setq-default show-trailing-whitespace t)
 
-;;c indent width
+;;indentation
 (setq-default c-basic-offset 2)
-
-;;spaces (t is for tabs)
+;;t for tabs
 (setq indent-tabs-mode nil)
-
-;;tab width
 (setq-default tab-width 4)
 
 (blink-cursor-mode 0)
@@ -35,9 +54,6 @@
 (global-set-key [f1] 'ansi-term)
 (global-set-key (kbd "M-o") 'other-window)
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(package-initialize)
 
 (add-hook 'org-mode-hook #'toggle-word-wrap)
 
@@ -53,7 +69,7 @@
  '(ansi-term-color-vector
    [unspecified "#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"])
  '(blink-cursor-mode nil)
- '(custom-enabled-themes (quote (gruvbox-light-soft)))
+ ;;'(custom-enabled-themes (quote (gruvbox-light-soft)))
  '(custom-safe-themes
    (quote
     ("f6a935e77513ba40014aa8467c35961fdb1fc936fa48407ed437083a7ad932de" "125fd2180e880802ae98b85f282b17f0aa8fa6cb9fc4f33d7fb19a38c40acef0" "dc9a8d70c4f94a28aafc7833f8d05667601968e6c9bf998791c39fcb3e4679c9" "5a970147df34752ed45bfdf0729233abfc085d9673ae7e40210c5e2d8f624b08" "65d9573b64ec94844f95e6055fe7a82451215f551c45275ca5b78653d505bc42" "357d5abe6f693f2875bb3113f5c031b7031f21717e8078f90d9d9bc3a14bcbd8" "c1390663960169cd92f58aad44ba3253227d8f715c026438303c09b9fb66cdfb" default)))
@@ -64,9 +80,6 @@
  '(package-selected-packages
    (quote
     (gruvbox-theme flycheck helm evil-org mu4e-alert haskell-snippets haskell-mode yasnippet auto-complete-clang-async auto-complete python-environment magit gnu-apl-mode expand-region evil)))
- '(send-mail-function (quote smtpmail-send-it))
- '(smtpmail-smtp-server "mail.cock.li")
- '(smtpmail-smtp-service 465)
  '(tool-bar-mode nil))
 
 (custom-set-faces
