c375b3f7022407fcf67eb6e74b80d22bb063c2cd
Curtis Haig Gallagher <chg@localhost.localdomain>
2018-02-18 10:51:37 -0500

    added evil-magit

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -22,6 +22,7 @@
   (setq evil-want-C-u-scroll t)
   :config
   (evil-mode 1))
+(use-package evil-magit)
 (use-package flycheck
   :config
   (add-hook 'after-init-hook #'global-flycheck-mode))
@@ -145,3 +146,17 @@
 ; ;; Your init file should contain only one such instance.
 ; ;; If there is more than one, they won't work right.
 ; '(default ((t (:family "Liberation Mono" :foundry "1ASC" :slant normal :weight normal :height 103 :width normal)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(gruvbox-theme gnu-apl-mode magit python-environment haskell-snippets haskell-mode evil-org auto-complete yasnippet flycheck evil use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
